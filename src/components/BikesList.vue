64c88ee2cf43dc186d3b40301c61a9e717d0cb11
unknown <dgloria.web@gmail.com>
2020-05-04 16:18:18 +0100

    added bikes

--- src/components/BikesList.vue
+++ src/components/BikesList.vue
@@ -1,8 +1,34 @@
 <template>
   <div>
-    bikes list
-    <p>Frame: {{bike.results[0].Frame}}</p>
-    <p>Bars: {{bike.results[0].Bars}}</p>
+    <ul>
+      <li v-for="item in bike.results" :key="item.Name">
+        <ul>
+          <li>Name: {{ item.Name }}</li>
+          <li>Make: {{ item.Make }}</li>
+          <li>Frame: {{ item.Frame }}</li>
+          <li>Fork: {{ item.Fork }}</li>
+          <li>Headset: {{ item.Headset }}</li>
+          <li>Stem: {{ item.Stem }}</li>
+          <li>Bars: {{ item.Bars }}</li>
+          <li>Front Brake: {{ item.Front_Brake }}</li>
+          <li>Rear Brake: {{ item.Rear_Brake }}</li>
+          <li>Rear Mech: {{ item.Rear_Mech }}</li>
+          <li>Shifters: {{ item.Shifters }}</li>
+          <li>Speed: {{ item.Speed }}</li>
+          <li>Rims: {{ item.Rims }}</li>
+          <li>Front Tyre: {{ item.Front_Tyre }}</li>
+          <li>Rear Tyre: {{ item.Rear_Tyre }}</li>
+          <li>Seat Post: {{ item.Seat_Post }}</li>
+          <li>Saddle: {{ item.Saddle }}</li>
+          <li>Chainset: {{ item.Chainset }}</li>
+          <li>Bottom Bracket: {{ item.Bottom_Bracket }}</li>
+          <li>Chain: {{ item.Chain }}</li>
+          <li>Cassette: {{ item.Cassette }}</li>
+          <li>Pedals: {{ item.Pedals }}</li>
+          <li></li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +37,30 @@ export default {
   name: 'BikesList',
   data() {
     return {
-      bike: {},
+      bike: {
+        Name: '',
+        Make: '',
+        Frame: '',
+        Fork: '',
+        Headset: '',
+        Stem: '',
+        Bars: '',
+        Front_Brake: '',
+        Rear_Brake: '',
+        Rear_Mech: '',
+        Shifters: '',
+        Speed: '',
+        Rims: '',
+        Front_Tyre: '',
+        Rear_Tyre: '',
+        Seat_Post: '',
+        Saddle: '',
+        Chainset: '',
+        Bottom_Bracket: '',
+        Chain: '',
+        Cassette: '',
+        Pedals: '',
+      },
     }
   },
   created: function() {
@@ -37,7 +86,7 @@ export default {
 </script>
 
 <style scoped>
-p{
-  text-align:left;
+li {
+  text-align: left;
 }
-  </style>
+</style>
