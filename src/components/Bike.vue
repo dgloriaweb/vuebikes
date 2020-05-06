acfe3e1ddfe0fb306d4a4f3a38ecf870e396f818
unknown <dgloria.web@gmail.com>
2020-05-06 19:07:22 +0100

    fixed image

---
+++ src/components/Bike.vue
@@ -0,0 +1,117 @@
+<template>
+  <router-link :to="bikePath">
+    <table>
+      <tr>
+        <td>Name:</td>
+        <td>{{ bike.Name }}</td>
+      </tr>
+      <tr>
+        <td>Make:</td>
+        <td>{{ bike.Make }}</td>
+      </tr>
+      <tr>
+        <td>Frame:</td>
+        <td>{{ bike.Frame }}</td>
+      </tr>
+      <tr>
+        <td>Fork:</td>
+        <td>{{ bike.Fork }}</td>
+      </tr>
+      <tr>
+        <td>Headset:</td>
+        <td>{{ bike.Headset }}</td>
+      </tr>
+      <tr>
+        <td>Stem:</td>
+        <td>{{ bike.Stem }}</td>
+      </tr>
+      <tr>
+        <td>Bars:</td>
+        <td>{{ bike.Bars }}</td>
+      </tr>
+      <tr>
+        <td>Front Brake:</td>
+        <td>{{ bike.Front_Brake }}</td>
+      </tr>
+      <tr>
+        <td>Rear Brake:</td>
+        <td>{{ bike.Rear_Brake }}</td>
+      </tr>
+      <tr>
+        <td>Rear Mech:</td>
+        <td>{{ bike.Rear_Mech }}</td>
+      </tr>
+      <tr>
+        <td>Shifters:</td>
+        <td>{{ bike.Shifters }}</td>
+      </tr>
+      <tr>
+        <td>Speed:</td>
+        <td>{{ bike.Speed }}</td>
+      </tr>
+      <tr>
+        <td>Rims:</td>
+        <td>{{ bike.Rims }}</td>
+      </tr>
+      <tr>
+        <td>Front Tyre:</td>
+        <td>{{ bike.Front_Tyre }}</td>
+      </tr>
+      <tr>
+        <td>Rear Tyre:</td>
+        <td>{{ bike.Rear_Tyre }}</td>
+      </tr>
+      <tr>
+        <td>Seat Post:</td>
+        <td>{{ bike.Seat_Post }}</td>
+      </tr>
+      <tr>
+        <td>Saddle:</td>
+        <td>{{ bike.Saddle }}</td>
+      </tr>
+      <tr>
+        <td>Chainset:</td>
+        <td>{{ bike.Chainset }}</td>
+      </tr>
+      <tr>
+        <td>Bottom Bracket:</td>
+        <td>{{ bike.Bottom_Bracket }}</td>
+      </tr>
+      <tr>
+        <td>Chain:</td>
+        <td>{{ bike.Chain }}</td>
+      </tr>
+      <tr>
+        <td>Cassette:</td>
+        <td>{{ bike.Cassette }}</td>
+      </tr>
+      <tr>
+        <td>Pedals:</td>
+        <td>{{ bike.Pedals }}</td>
+      </tr>
+    </table>
+
+    <img :src="Image" :alt="bike.Name" />
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ['bike'],
+  computed: {
+    Image: function() {
+      return `${this.bike.Image}`
+    },
+    bikePath: function() {
+      return `/bike/${this.bike.ID}`
+    },
+  },
+}
+</script>
+
+<style scoped>
+img {
+  box-shadow: 0 0 35px #111;
+  max-width: 11rem;
+}
+</style>
--- src/components/BikesList.vue
+++ src/components/BikesList.vue
@@ -1,78 +1,19 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in bike.results" :key="item.Name">
-        <ul>
-          <li>Name: {{ item.Name }}</li>
-          <li>Make: {{ item.Make }}</li>
-          <li>Frame: {{ item.Frame }}</li>
-          <li>Fork: {{ item.Fork }}</li>
-          <li>Headset: {{ item.Headset }}</li>
-          <li>Stem: {{ item.Stem }}</li>
-          <li>Bars: {{ item.Bars }}</li>
-          <li>Front Brake: {{ item.Front_Brake }}</li>
-          <li>Rear Brake: {{ item.Rear_Brake }}</li>
-          <li>Rear Mech: {{ item.Rear_Mech }}</li>
-          <li>Shifters: {{ item.Shifters }}</li>
-          <li>Speed: {{ item.Speed }}</li>
-          <li>Rims: {{ item.Rims }}</li>
-          <li>Front Tyre: {{ item.Front_Tyre }}</li>
-          <li>Rear Tyre: {{ item.Rear_Tyre }}</li>
-          <li>Seat Post: {{ item.Seat_Post }}</li>
-          <li>Saddle: {{ item.Saddle }}</li>
-          <li>Chainset: {{ item.Chainset }}</li>
-          <li>Bottom Bracket: {{ item.Bottom_Bracket }}</li>
-          <li>Chain: {{ item.Chain }}</li>
-          <li>Cassette: {{ item.Cassette }}</li>
-          <li>Pedals: {{ item.Pedals }}</li>
-          <li>Image: {{ item.Image }}</li>
-          <img :src="item.Image" />
-          <li></li>
-        </ul>
-      </li>
-    </ul>
+    <div v-for="bike in bikes" :key="bike.id">
+      <Bike :bike="bike" />
+    </div>
   </div>
 </template>
 
 <script>
+import Bike from './Bike.vue'
 export default {
   name: 'BikesList',
   data() {
     return {
-      bike: {
-        Name: '',
-        Make: '',
-        Frame: '',
-        Fork: '',
-        Headset: '',
-        Stem: '',
-        Bars: '',
-        Front_Brake: '',
-        Rear_Brake: '',
-        Rear_Mech: '',
-        Shifters: '',
-        Speed: '',
-        Rims: '',
-        Front_Tyre: '',
-        Rear_Tyre: '',
-        Seat_Post: '',
-        Saddle: '',
-        Chainset: '',
-        Bottom_Bracket: '',
-        Chain: '',
-        Cassette: '',
-        Pedals: '',
-        Image: 'image/jpg',
-      },
+      bikes: [],
     }
-  },
-  computed: {
-    image: function() {
-      return `${this.bike.image}`
-    },
-    moviePath: function() {
-      return `/movie/${this.movie.id}`
-    },
   },
   created: function() {
     this.fetchData()
@@ -81,17 +22,19 @@ export default {
     fetchData: async function() {
       try {
         const res = await fetch(
-          // 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ec5dc58dd49114aa47aebd1659fef849'
           // 'https://herokutestnodecrashcourse.herokuapp.com/bikes.json'
           //the above didn't work because of the browser's CORS policy, with cors_anywhere extension, this works.
           'https://limitless-peak-43368.herokuapp.com/https://herokutestnodecrashcourse.herokuapp.com/bikes.json'
         )
-        const bike = await res.json()
-        this.bike = bike
+        const bikes = await res.json()
+        this.bikes = bikes.results
       } catch (e) {
         console.log(e)
       }
     },
+  },
+  components: {
+    Bike,
   },
 }
 </script>
